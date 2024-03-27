d7f6a8d50ac1441bc8da78af7400873e4c7af401
Ankit Chaudhary <ankitcha810@gmail.com>
2024-03-27 16:29:50 +0530

    Test: Done With GetSigned Urls with S3

--- apps/server/src/routes/workspace.ts
+++ apps/server/src/routes/workspace.ts
@@ -1,6 +1,15 @@
+import Auth from "../middleware/auth";
 import { prisma } from "../prisma";
 import express from "express";
+import WorkspaceService from "../services/workspace";
 
 const router = express.Router();
+
+// editor Logic
+router
+  .route("/upload-video")
+  .post(Auth.editorAuth, WorkspaceService.uploadVideo);
+
+router.route("/get-signed-url").get(WorkspaceService.GetSignedUrlForVideo);
 
 export default router;
---
+++ apps/server/src/services/workspace.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { prisma } from "../prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_USER_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_USER_SECRET_KEY as string,
+  },
+  region: "ap-south-1",
+});
+
+class WorkspaceService {
+  public static async uploadVideo(req: Request, res: Response) {
+    try {
+    } catch (error) {}
+  }
+
+  public static async GetSignedUrlForVideo(req: Request, res: Response) {
+    try {
+      const acutalUserId = "12345677";
+      const pubtobjectCommand = new PutObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: `uploads/${acutalUserId}/video/${Date.now().toString()}`,
+      });
+      const signedUrl = await getSignedUrl(s3Client, pubtobjectCommand);
+      return res.json(signedUrl);
+    } catch (error) {
+      console.log("Error in GetSignedUrlForVideo", error);
+      return res.status(501).json({ msg: "Internal Server Error" });
+    }
+  }
+}
+
+export default WorkspaceService;
