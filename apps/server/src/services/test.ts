31bd197a188a5777ddc5b599c3449dbe95a85091
Ankit Chaudhary <ankitcha810@gmail.com>
2024-04-09 16:59:23 +0530

    Error: Invalid credentials format.

--- apps/server/src/services/test.ts
+++ apps/server/src/services/test.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosDefaults } from "axios";
+import { oauth2Client } from "../utility";
 
 const fs = require("fs");
 const readline = require("readline");
@@ -12,16 +13,21 @@ const categoryIds = {
 };
 
 const SCOPES = ["https://www.googleapis.com/auth/youtube.upload"];
+const TOKEN_PATH = "../" + "client_oauth_token.json";
 
+const oauth_path = "../../apps/server/client_secret.json";
 export function uploadVideo() {
-  const oauth2Client = new OAuth2(
-    process.env.OAUTH_CLIENT_ID,
-    process.env.OAUTH_CLIENT_SECRET,
-    process.env.REDIRECT_URL
-  );
+  fs.readFile(
+    oauth_path,
+    function processClientSecrets(err: any, content: any) {
+      if (err) {
+        console.log("Error loading client secret file: " + err);
+        return;
+      }
 
-  // Authorize the client and upload the video
-  authorize(oauth2Client, (auth: any) => uploadVideo1(auth));
+      authorize(JSON.parse(content), (auth: any) => uploadVideo1(auth));
+    }
+  );
 }
 
 async function uploadVideo1(auth: any) {
@@ -54,13 +60,65 @@ async function uploadVideo1(auth: any) {
 }
 
 function authorize(credentials: any, callback: any) {
-  const oauth2Client = new OAuth2();
+  console.log(credentials);
+  console.log("token path", TOKEN_PATH);
+  if (
+    !credentials ||
+    !credentials.installed ||
+    !credentials.installed.client_secret ||
+    !credentials.installed.client_id
+  ) {
+    console.error("Invalid credentials format.");
+    return;
+  }
 
-  oauth2Client.setCredentials({
-    access_token: process.env.ACCESS_TOKEN,
-    refresh_token: process.env.REFRESH_TOKEN,
+  const clientSecret = credentials.installed.client_secret;
+  const clientId = credentials.installed.client_id;
+  const redirectUrl = credentials.installed.redirect_uris[0];
+  const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function (err: any, token: any) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+function getNewToken(credentials: any, callback: any) {
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline",
     scope: SCOPES,
   });
+  console.log("Authorize this app by visiting this url: ", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question("Enter the code from that page here: ", function (code: any) {
+    rl.close();
+    oauth2Client.getToken(code, function (err, token) {
+      if (err) {
+        console.log("Error while trying to retrieve access token", err);
+        return;
+      }
+      if (!token) {
+        console.log("No token received.");
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
 
-  callback(oauth2Client);
+function storeToken(token: any) {
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err: any) => {
+    if (err) throw err;
+    console.log("Token stored to" + TOKEN_PATH);
+  });
 }
