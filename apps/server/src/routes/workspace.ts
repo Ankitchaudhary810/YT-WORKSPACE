7d2bdc4d15fc3df88aafd4cc77c2150fd210fab3
Ankit Chaudhary <ankitcha810@gmail.com>
2024-04-10 08:37:55 +0530

    Remove Test

--- apps/server/src/routes/workspace.ts
+++ apps/server/src/routes/workspace.ts
@@ -12,25 +12,4 @@ router
   .route("/upload-video")
   .get(Auth.editorAuth, upload.single("file"), WorkspaceService.uploadVideo);
 
-// Testing Mode.
-router.get("/upload-to-youtube", async (req: any, res: any) => {
-  try {
-    await uploadVideo();
-    res.status(200).send("Video uploaded successfully.");
-  } catch (error) {
-    console.error("Error uploading video:", error);
-    res.status(500).send("Failed to upload video.");
-  }
-});
-
-router.post("/upload-video-testing", async (req: any, res: any) => {
-  try {
-    await uploadVideo();
-    res.status(200).send("Video uploaded successfully.");
-  } catch (error) {
-    console.error("Error uploading video:", error);
-    res.status(500).send("Failed to upload video.");
-  }
-});
-
 export default router;
