5bb028a008b2598ab5c8cc774b400ec4149dbdd1
Ankit Chaudhary <ankitcha810@gmail.com>
2024-04-17 18:28:30 +0530

    refactor

--- apps/client/app/workspace/[id]/page.tsx
+++ apps/client/app/workspace/[id]/page.tsx
@@ -1,18 +1,112 @@
+"use client";
+import Loader from "@/components/ui/Loader";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useVideoById, useupdatevideo } from "@/hooks/video";
+import { useState } from "react";
+import { FaYoutube } from "react-icons/fa6";
+import { MdOutlinePublishedWithChanges } from "react-icons/md";
 import React from "react";
-import VideoDetails from "./VideoDetails";
-import { Metadata } from "next";
+import { useMutation } from "@tanstack/react-query";
 
-const page = () => {
+interface Props {
+  params: { id: string };
+}
+
+const Page = ({ params: { id } }: Props) => {
+  const { video, isLoading } = useVideoById(id);
+
+  const [videoData, setVideoData] = useState({
+    title: video?.title || "",
+    description: video?.description || "",
+  });
+
+  const updateVideoMutation = useupdatevideo(
+    videoData.title,
+    videoData.description,
+    id
+  );
+
+  const handleChangeInput = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setVideoData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  React.useEffect(() => {
+    if (!isLoading && video) {
+      setVideoData({
+        title: video.title,
+        description: video.description,
+      });
+    }
+  }, [isLoading, video]);
+
+  const useUpdateMutation = useMutation({
+    mutationFn: useupdatevideo,
+  });
+
+  const handleChanges = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    await useUpdateMutation.mutate;
+  };
+
+  if (isLoading) {
+    return (
+      <h1 className="flex items-center justify-center">
+        <Loader />
+      </h1>
+    );
+  }
+
   return (
-    <>
-      <VideoDetails params={{ id: "clulbaahp0000125zrfr6cc33" }} />
-    </>
+    <div className="container mx-auto mt-8 grid grid-cols-1 sm:grid-cols-2 gap-8 text-black">
+      <div>
+        <h1 className="text-2xl font-bold mb-4 text-white">{video?.title}</h1>
+        <video className="w-full" controls muted>
+          <source src={video?.aws_s3_url} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div>
+        <Card className="p-3 bg-black sm:mt-10">
+          <div className="flex flex-col gap-4">
+            <Input
+              type="text"
+              value={videoData.title}
+              onChange={handleChangeInput}
+              name="title"
+              className="border rounded px-4 py-2"
+            />
+            <Textarea
+              value={videoData.description}
+              onChange={handleChangeInput}
+              name="description"
+              className="border border-gray-300 rounded px-4 py-2 h-40"
+            />
+            <Button variant={"outline"} type="button" onClick={handleChanges}>
+              {updateVideoMutation.isLoading ? <Loader /> : "Save Changes"}
+
+              <MdOutlinePublishedWithChanges
+                className={`ml-1 ${updateVideoMutation.isLoading ? "hidden" : "block"}`}
+                size={28}
+              />
+            </Button>
+            <Button type="button" variant={"secondary"}>
+              Upload
+              <FaYoutube color="red" className="ml-1" size={25} />
+            </Button>
+          </div>
+        </Card>
+      </div>
+    </div>
   );
 };
 
-export const metadata: Metadata = {
-  title: "dynamic title",
-  description: "dynamic descrition",
-};
-
-export default page;
+export default Page;
--- apps/client/hooks/video.ts
+++ apps/client/hooks/video.ts
@@ -25,43 +25,66 @@ export const useVideoById = (id: string) => {
   return { ...query, video: query.data, isLoading: query.isLoading };
 };
 
-export const useUpdateVideoById = (id: string) => {
-  const mutation = useMutation({
-    mutationKey: ["update-workspace-by-id"],
-    mutationFn: async ({
-      title,
-      description,
-    }: {
-      title: string;
-      description: string;
-    }) => {
-      const response = await fetch(
-        process.env.NEXT_PUBLIC_BACKEND_URL + "/update-workspace-by-id/" + id,
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("user_jwt")}`,
-          },
-          body: JSON.stringify({ title, description }),
-          cache: "no-cache",
-        }
-      );
-      console.log(response);
-      if (!response.ok) {
-        throw new Error("Failed to find the current user");
-      }
-      return await response.json();
-    },
-    onMutate: async () => {
-      toast.loading("Updating...", { id: "1" });
-    },
-    onSuccess: async () => {
-      toast.success("Updated", { id: "1" });
-    },
-    onError: async () => {
-      toast.error("Fail to update", { id: "1" });
-    },
-  });
+// export const useUpdateVideoById = (id: string) => {
+//   const mutation = useMutation({
+//     mutationKey: ["update-workspace-by-id"],
+//     mutationFn: async ({
+//       title,
+//       description,
+//     }: {
+//       title: string;
+//       description: string;
+//     }) => {
+//       const response = await fetch(
+//         process.env.NEXT_PUBLIC_BACKEND_URL + "/update-workspace-by-id/" + id,
+//         {
+//           method: "POST",
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem("user_jwt")}`,
+//           },
+//           body: JSON.stringify({ title, description }),
+//           cache: "no-cache",
+//         }
+//       );
+//       console.log(response);
+//       if (!response.ok) {
+//         throw new Error("Failed to find the current user");
+//       }
+//       return await response.json();
+//     },
+//     onMutate: async () => {
+//       toast.loading("Updating...", { id: "1" });
+//     },
+//     onSuccess: async () => {
+//       toast.success("Updated", { id: "1" });
+//     },
+//     onError: async () => {
+//       toast.error("Fail to update", { id: "1" });
+//     },
+//   });
 
-  return { ...mutation, data: mutation.data };
+//   return { ...mutation, data: mutation.data };
+// };
+
+// extra
+
+export const useupdatevideo = async (
+  title: string,
+  description: string,
+  id: string
+) => {
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_BACKEND_URL + "/update-workspace-by-id/" + id,
+    {
+      method: "POST",
+      headers: {
+        "ContenType-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("user_jwt")}`,
+      },
+      body: JSON.stringify({ title, description }),
+      cache: "no-cache",
+    }
+  );
+
+  return response.json();
 };
