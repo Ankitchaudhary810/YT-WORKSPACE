739b0c9626d9ad315425028bd04a22439f881c0c
Ankit Chaudhary <ankitcha810@gmail.com>
2024-04-20 18:15:52 +0530

    Added: about page image

--- apps/client/app/about/page.tsx
+++ apps/client/app/about/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import about from "../../public/about.jpg";
+import Image from "next/image";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="flex  gap-2 justify-evenly mt-20">
+      <div>
+        <Image
+          src={about}
+          alt="about-page"
+          width={500}
+          className="rounded-sm"
+        />
+      </div>
+      <div>Paragraph.</div>
+    </div>
+  );
 };
 
 export default AboutPage;
