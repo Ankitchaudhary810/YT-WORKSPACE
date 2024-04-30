dec4f1a19486d835635ee2e0f1e2afcc752b784e
Ankit Chaudhary <ankitcha810@gmail.com>
2024-04-30 10:06:32 +0530

    Added Editor Login Ui

--- apps/editor/app/page.tsx
+++ apps/editor/app/page.tsx
@@ -1,9 +1,62 @@
 import Image from "next/image";
+import "./globals.css";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Working....
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="bg-zinc-900">
+            Continue
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px] bg-zinc-950 text-slate-100">
+          <DialogHeader>
+            <DialogTitle>Login</DialogTitle>
+            <DialogDescription>
+              Login To workspace In order to upload the video.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="email" className="text-left">
+                Email
+              </Label>
+              <Input
+                id="email"
+                placeholder="kit@gmail.com"
+                className="col-span-3"
+              />
+            </div>
+
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-left">
+                Password
+              </Label>
+              <Input
+                id="name"
+                placeholder="***** *****"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Submit.</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </main>
   );
 }
